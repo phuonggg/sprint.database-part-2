7981b9a031d031bb620326d71fe4651501267e13
CarlosIUSalazar <carlosiusalazar@gmail.com>
2020-04-23 17:51:02 +0900

    Initial commit
    Co-authored-by: Phuonggg <phuonggg@users.noreply.github.com>

--- models/users/list.js
+++ models/users/list.js
@@ -1,5 +1,16 @@
+/* eslint-disable prettier/prettier */
 module.exports = (knex, User) => {
-  return () => {
-    return Promise.resolve([]); // fix me!
+  return async () => {
+    const allUsers = await knex.select("id", "username").from("users"); // fix me!
+    console.log("ALLUSEEEERS", allUsers);
+    return allUsers;
+
+    //const singleUsers = allUsers.map(user => user)
+    //console.log("SERIALIZEEEE", allUsers[0].serialize())
+    // let resultObj = {};
+    // allUsers.forEach((item) => resultObj = item)
+    // console.log("RESULTOBJJJJ", resultObj)
+
+    // allUsers.map(user => user.id);
   };
 };
